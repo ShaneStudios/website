5a4794fb04bb9d4be3837e891c7116c60edb8a55
Shane Studios <shanestudiosofficial@gmail.com>
2025-08-02 19:22:47 -0400

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,9 @@
+export const menuItems = [
+    { href: 'index.html', icon: 'fas fa-home', text: 'Home' },
+    { href: 'projects.html', icon: 'fas fa-project-diagram', text: 'Projects' },
+    { href: 'snippets.html', icon: 'fas fa-file-code', text: 'Snippets' },
+    { href: 'articles.html', icon: 'fas fa-newspaper', text: 'Articles' },
+    { href: 'community.html', icon: 'fas fa-users', text: 'Community' },
+    { href: 'contact.html', icon: 'fas fa-envelope', text: 'Contact' },
+    { href: 'reviews.html', icon: 'fas fa-star', text: 'Reviews' }
+];
