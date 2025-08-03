a12a5b961c77176131cd16390249ffeb37be93e3
Shane Studios <shanestudiosofficial@gmail.com>
2025-08-03 14:09:23 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
 export const menuItems = [
-    { href: 'index.html', icon: 'fas fa-home', text: 'Home' },
-    { href: 'projects.html', icon: 'fas fa-project-diagram', text: 'Projects' },
-    { href: 'snippets.html', icon: 'fas fa-file-code', text: 'Snippets' },
-    { href: 'articles.html', icon: 'fas fa-newspaper', text: 'Articles' },
-    { href: 'community.html', icon: 'fas fa-users', text: 'Community' },
-    { href: 'users.html', icon: 'fas fa-users-line', text: 'Users' },
-    { href: 'profile.html', icon: 'fas fa-user-circle', text: 'Profile' },
-    { href: 'contact.html', icon: 'fas fa-envelope', text: 'Contact' },
-    { href: 'reviews.html', icon: 'fas fa-star', text: 'Reviews' }
+    { href: 'website/index.html', icon: 'fas fa-home', text: 'Home' },
+    { href: 'website/projects.html', icon: 'fas fa-project-diagram', text: 'Projects' },
+    { href: 'website/snippets.html', icon: 'fas fa-file-code', text: 'Snippets' },
+    { href: 'website/articles.html', icon: 'fas fa-newspaper', text: 'Articles' },
+    { href: 'website/community.html', icon: 'fas fa-users', text: 'Community' },
+    { href: 'website/users.html', icon: 'fas fa-users-line', text: 'Users' },
+    { href: 'website/profile.html', icon: 'fas fa-user-circle', text: 'Profile' },
+    { href: 'website/contact.html', icon: 'fas fa-envelope', text: 'Contact' },
+    { href: 'website/reviews.html', icon: 'fas fa-star', text: 'Reviews' }
 ];
