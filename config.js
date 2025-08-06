82d6fb68d517d4628c8901e704980dadf4bf9550
Shane Studios <shanestudiosofficial@gmail.com>
2025-08-05 21:40:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
-export const SUPABASE_URL = 'https://uopfcevwjczrcpuwyqyx.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvcGZjZXZ3amN6cmNwdXd5cXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMjg5MjQsImV4cCI6MjA2OTgwNDkyNH0.1dR-R92yVX5ASt2Ngh4SzB6jBJnNZdn0-K0wMzupqRc';
-export const TURNSTILE_SITE_KEY = '0x4AAAAAABn-7SFWEu0ZnLgi';
+export const firebaseConfig = {
+    apiKey: "AIzaSyDw_wwJ9prY4h0rqEqTrXn-5xgVPJc-lWU",
+    authDomain: "website-8a238.firebaseapp.com",
+    projectId: "website-8a238",
+    storageBucket: "website-8a238.firebasestorage.app",
+    messagingSenderId: "857607060072",
+    appId: "1:857607060072:web:aab37005f45f6157f259dd"
+};
+export const RECAPTCHA_SITE_KEY = '6Lfn_5srAAAAANoHNAN2orcrloed7TkeDRMhKJuo';
